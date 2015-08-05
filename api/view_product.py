29c58408628924d155ff5471c424f7bf522c90fc
Shirish <shirish542@gmail.com>
2015-08-05 16:56:20 +0530

    create product api is customized

--- api/view_product.py
+++ api/view_product.py
@@ -1,10 +1,11 @@
-from yourguy.models import Product, VendorAgent
+from yourguy.models import Product, VendorAgent, Vendor
 
-# from django.contrib.auth.decorators import permission_required, group_required
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, authentication
 from rest_framework import viewsets
 from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
 
 from api.serializers import ProductSerializer
 from api.views import user_role
@@ -21,17 +22,44 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    # @group_required(constants.VENDOR)
-    # @permission_required(constants.VENDOR)
     def list(self, request):
-    	role = user_role(request.user)
-    	if role == constants.VENDOR:
-    		vendor_agent = VendorAgent.objects.get(user = self.request.user)
-    		products_of_vendor = Product.objects.filter(vendor = vendor_agent.vendor)
-    		serializer = ProductSerializer(products_of_vendor, many=True)
-    		return Response(serializer.data, status=status.HTTP_201_CREATED)
-    	else:
-    		content = {'error':'You dont have permissions to view all vendors'}
-    		return Response(content, status = status.HTTP_400_BAD_REQUEST)
+        role = user_role(request.user)
+        if role == constants.VENDOR:
+            vendor_agent = VendorAgent.objects.get(user = self.request.user)
+            products_of_vendor = Product.objects.filter(vendor = vendor_agent.vendor)
+            serializer = ProductSerializer(products_of_vendor, many=True)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            content = {'error':'You dont have permissions to view products'}
+            return Response(content, status = status.HTTP_400_BAD_REQUEST)
+    
+    def create(self, request):
+        role = user_role(request.user)
+        if role == constants.VENDOR:
+            vendor_agent = get_object_or_404(VendorAgent, user = self.request.user)
+            vendor = vendor_agent.vendor
+            
+            try:
+                name = request.data['name']
+                description = request.data['description']
+                cost_string = request.data['cost']
+                cost = float(cost_string)
+            except:
+                content = {'error':'missing params with name, description, cost, vendor'}
+                return Response(content, status = status.HTTP_400_BAD_REQUEST)
 
+            new_product = Product.objects.create(vendor = vendor,
+                name = name, 
+                description = description, 
+                cost = cost)
+
+            content = {
+            'product_id':new_product.id,
+            'description': 'product added successfully'
+            }
+
+            return Response(content, status = status.HTTP_200_OK)
+        else:
+            content = {'error':'You dont have permissions to add a product'}
+            return Response(content, status = status.HTTP_400_BAD_REQUEST)
 
