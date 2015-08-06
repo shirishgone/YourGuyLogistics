71deea78a4bfa02d19fc97a9a345479052023b48
Shirish <shirish542@gmail.com>
2015-08-06 11:36:37 +0530

    area names sorted

--- api/view_area.py
+++ api/view_area.py
@@ -1,10 +1,9 @@
-from yourguy.models import Area
-
+from django.db.models.functions import Lower
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, authentication
 from rest_framework import viewsets
-
 from api.serializers import AreaSerializer
+from yourguy.models import Area
 
 class AreaViewSet(viewsets.ModelViewSet):
     """
@@ -15,3 +14,7 @@ class AreaViewSet(viewsets.ModelViewSet):
 
     queryset = Area.objects.all()
     serializer_class = AreaSerializer
+    
+    def get_queryset(self):
+        queryset = Area.objects.order_by(Lower('area_name'))
+        return queryset
