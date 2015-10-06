2453f0d20f221646fbd3d42064345864bfa08d18
Shirish <shirish542@gmail.com>
2015-10-06 21:10:56 +0530

    consumers v2 api updated

--- api_v2/view_consumers.py
+++ api_v2/view_consumers.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.models import User, Group
 from django.shortcuts import get_object_or_404
 from django.db.models.functions import Lower
@@ -19,6 +18,36 @@ import datetime
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import constants
         
+def consumer_list_dict(consumer):
+    consumer_dict = {
+            'id' : consumer.id,
+            'name':consumer.user.first_name,
+            'phone_number':consumer.user.username
+            }
+    return consumer_dict
+
+
+def consumer_detail_dict(consumer):
+    consumer_dict = {
+            'id' : consumer.id,
+            'name':consumer.user.first_name,
+            'phone_number':consumer.user.username,
+            "addresses":[]
+            }
+    
+    all_addresses = consumer.addresses.all()
+    for address in all_addresses:
+        adr_dict = {
+        "id":address.id,
+        "full_address":address.full_address,
+        "landmark":address.landmark,
+        "pin_code":address.pin_code
+        }   
+        consumer_dict['addresses'].append(adr_dict)
+
+    return consumer_dict
+
+
 class ConsumerViewSet(viewsets.ModelViewSet):
     """
     Consumer viewset that provides the standard actions 
@@ -26,6 +55,28 @@ class ConsumerViewSet(viewsets.ModelViewSet):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Consumer.objects.all()
+
+    def retrieve(self, request, pk = None):        
+        consumer = get_object_or_404(Consumer, id = pk)
+        role = user_role(request.user)
+        if role == 'vendor':
+            vendor_agent = get_object_or_404(VendorAgent, user = request.user)
+            all_associated_vendors = consumer.associated_vendor.all()
+            is_consumer_associated_to_vendor = False
+            for vendor in all_associated_vendors:
+                if vendor.id == vendor_agent.vendor.id:
+                    is_consumer_associated_to_vendor = True
+                    break
+            if is_consumer_associated_to_vendor:
+                detail_dict = consumer_detail_dict(consumer)
+                response_content = { "data": detail_dict}
+                return Response(response_content, status = status.HTTP_200_OK)
+            else:
+                content = {'error':'You dont have permissions to view this consumer.'}
+                return Response(content, status = status.HTTP_400_BAD_REQUEST)  
+        else:
+            content = {'error':'You dont have permissions to view this consumer.'}
+            return Response(content, status = status.HTTP_400_BAD_REQUEST)  
 
     def list(self, request):
         page = self.request.QUERY_PARAMS.get('page', None)
@@ -42,33 +93,8 @@ class ConsumerViewSet(viewsets.ModelViewSet):
 
             result = []
             for consumer in customers:
-                result_consumer = {
-                "id":consumer.id,
-                "name":consumer.user.first_name,
-                "phone_number":consumer.user.username,
-                "addresses":[]
-                }
-
-                all_addresses = consumer.addresses.all()
-                for address in all_addresses:
-                    adr = {
-                        "id":address.id,
-                        "flat":address.flat_number,
-                        "building":address.building,
-                        "street":address.street,
-                        "landmark":address.landmark,
-                        "pin_code":address.pin_code
-                    }
-                    area = address.area
-                    if area is not None:
-                        adr["area_name"] = address.area.area_name
-                        adr["area_code"] = address.area.area_code
-                    else:
-                        adr["area_name"] = None
-                        adr["area_code"] = None
-
-                    result_consumer['addresses'].append(adr)
-                result.append(result_consumer)
+                consumer_dict = consumer_list_dict(consumer)
+                result.append(consumer_dict)
         
             response_content = { "data": result, "total_pages": total_pages }
             return Response(response_content, status = status.HTTP_200_OK)
