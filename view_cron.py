553fda6c26e1d3574b2e11189c02451827469ca0
Shirish <shirish542@gmail.com>
2015-10-26 21:26:11 +0530

    added scheduler for reporting

--- view_cron.py
+++ view_cron.py
@@ -10,6 +10,7 @@ from pytz import utc
 
 scheduler = BackgroundScheduler(timezone=utc)
 
+# AUTO ASSIGNED SCHEDULER ----------------------------------------
 def run_cron_assign_dg():
 	print('run_cron_assign_dg method is called.')
 	url = 'http://yourguy.herokuapp.com/api/v2/cron/'
@@ -19,15 +20,26 @@ def run_cron_assign_dg():
 	except:
 		print "Calling CRON JOB except case"
 
-# @scheduler.scheduled_job('interval', minutes = 1)
-# def timed_job():
-# 	print('This job is run every three minutes.')
-	#run_cron_assign_dg()
+
 
 @scheduler.scheduled_job('cron', hour = 0)
 def scheduled_job():
 	print('Scheduled job.')
 	run_cron_assign_dg()
+# ----------------------------------------------------------------
+
+# REPORTING SCHEDULER --------------------------------------------
+def send_daily_report():
+	url = 'http://yourguy.herokuapp.com/api/v2/daily_report/'
+	try:
+		result = requests.get(url)
+	except:
+		print "Error running CRON JOB Daily report"
+
+@scheduler.scheduled_job('reporting_cron', hour = 16)
+def scheduled_job_reporting():
+	send_daily_report()
+# ----------------------------------------------------------------
 
 scheduler.start()
 print('Scheduler started.')
