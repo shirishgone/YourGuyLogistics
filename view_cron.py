a3c417a94f4c8889a81175af61b15fb8a58702ab
Shirish <shirish542@gmail.com>
2015-10-27 20:46:43 +0530

    cron update for 9.30 IST

--- view_cron.py
+++ view_cron.py
@@ -21,7 +21,7 @@ def auto_assign():
 # ----------------------------------------------------------------
 
 # REPORTING SCHEDULER --------------------------------------------
-@scheduler.scheduled_job('cron', id = 'daily_report', hour = 14)
+@scheduler.scheduled_job('cron', id = 'daily_report', hour = 16)
 def daily_report():
 	print 'send_daily_report'
 	url = 'http://yourguy.herokuapp.com/api/v2/daily_report/'
