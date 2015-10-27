2173521302cdf77865c9cb4a6e0c17197dda6216
Shirish <shirish542@gmail.com>
2015-10-27 23:29:18 +0530

    indentation issue fixed

--- view_cron.py
+++ view_cron.py
@@ -1,16 +1,14 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
 
-#from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 import requests
 import base64
 import json
 from pytz import utc
 
-#scheduler = BackgroundScheduler(timezone=utc)
-scheduler = BlockingScheduler()
+scheduler = BackgroundScheduler(timezone=utc)
 
 # AUTO ASSIGNED SCHEDULER ----------------------------------------
 @scheduler.scheduled_job('cron', id='auto_assign_job_id', hour=0)
@@ -18,20 +16,18 @@ def auto_assign():
 	url = 'http://yourguy.herokuapp.com/api/v2/cron/'
 	try:
 		result = requests.get(url)
-	except Exception e:
-		print e
+	except:
 		# REPORT ERROR
 # ----------------------------------------------------------------
 
 # REPORTING SCHEDULER --------------------------------------------
-@scheduler.scheduled_job('cron', id='daily_report_job_id', hour=17, minute=55)
+@scheduler.scheduled_job('cron', id='daily_report_job_id', hour=18)
 def daily_report():
 	print 'send_daily_report'
 	url = 'http://yourguy.herokuapp.com/api/v2/daily_report/'
 	try:
 		result = requests.get(url)
-	except Exception e:
-		print e
+	except:
 		print 'send_daily_report : ERROR'
 		# REPORT ERROR
 # ----------------------------------------------------------------
