dda84652fd2ccb616de164f2bdfaf0f734d7ce09
Shirish <shirish542@gmail.com>
2015-10-27 22:37:47 +0530

    cron testing

--- view_cron.py
+++ view_cron.py
@@ -11,19 +11,19 @@ from pytz import utc
 scheduler = BackgroundScheduler(timezone=utc)
 
 # AUTO ASSIGNED SCHEDULER ----------------------------------------
-@scheduler.scheduled_job('cron', hour=0)
-def auto_assign():
-	url = 'http://yourguy.herokuapp.com/api/v2/cron/'
-	try:
-		result = requests.get(url)
-	except Exception e:
-		print e
-		# REPORT ERROR
+# @scheduler.scheduled_job('cron', hour=0)
+# def auto_assign():
+# 	url = 'http://yourguy.herokuapp.com/api/v2/cron/'
+# 	try:
+# 		result = requests.get(url)
+# 	except Exception e:
+# 		print e
+# 		# REPORT ERROR
 
 # ----------------------------------------------------------------
 
 # REPORTING SCHEDULER --------------------------------------------
-@scheduler.scheduled_job('cron', hour=17, minute=5)
+@scheduler.scheduled_job('cron', hour=17, minute=10)
 def daily_report():
 	print 'send_daily_report'
 	url = 'http://yourguy.herokuapp.com/api/v2/daily_report/'
