a103c24bcc80abf3ddb11d8ae91bd1c8384dad3b
Shirish <shirish542@gmail.com>
2015-11-23 15:37:03 +0530

    Dashboard api update for correct results

--- api/view_dashboard.py
+++ api/view_dashboard.py
@@ -12,7 +12,8 @@ from rest_framework.decorators import api_view
 
 from yourguy.models import Order, OrderDeliveryStatus, Consumer, Vendor, DeliveryGuy, Area, VendorAgent, Address, Product, OrderItem, User
 from datetime import datetime, timedelta, time
-from api.views import user_role, is_userexists, is_vendorexists, is_consumerexists, is_dgexists, is_address_exists, days_in_int, send_sms, normalize_offset_awareness
+from api.views import user_role, is_userexists, is_vendorexists, is_consumerexists, is_dgexists, is_address_exists, days_in_int, send_sms, normalize_offset_awareness, ist_day_start, ist_day_end
+from api.views import ist_datetime
 
 import constants
 import json
@@ -20,113 +21,124 @@ import json
 import recurrence
 from datetime import datetime
 from dateutil.rrule import rrule, DAILY
+from django.db.models import Sum
 
 def calculate_customers(vendor):
 	return 0
 
 @api_view(['POST'])
 def vendor_dashboard(request):
+
 	authentication_classes = [authentication.TokenAuthentication]
 	permission_classes = [IsAuthenticated]
 
+	# INPUT PARAMETERS ---------------------------------------------
 	try:
 		start_date_string = request.data['start_date']
 		end_date_string = request.data['end_date']	
-		vendor_id = request.data.get('vendor_id')
 		
-		# CHECK IF VENDOR WITH ID EXISTS
-		if vendor_id is not None:
-			try:
-				vendor = get_object_or_404(Vendor, pk = vendor_id)
-			except Exception, e:
-				content = {'error':'Invalid vendor_id'}
-				return Response(content, status = status.HTTP_400_BAD_REQUEST)
-
 		start_date = parse_datetime(start_date_string)
+		start_date = ist_day_start(start_date)
+		
 		end_date = parse_datetime(end_date_string)
+		end_date = ist_day_end(end_date)
+	
 	except Exception, e:
 		content = {'error':'Error in params: start_date, end_date'}
 		return Response(content, status = status.HTTP_400_BAD_REQUEST)
+	# ---------------------------------------------------------------	
 
+	# VENDOR INPUTS ---------------------------------------------------
+	vendor = None
 	role = user_role(request.user)
-	if (role == constants.VENDOR or role == constants.OPERATIONS):
-		total_orders_placed = 0
-		total_orders_delivered = 0
-		total_orders_cancelled = 0
-
-		total_cod_amount = 0
-		total_sales = 0
-		orders_graph = []
-
-		# CREATE DATE RULE ==================
-		rule_daily = rrule(DAILY, dtstart=start_date, until=end_date)
-		alldates = list(rule_daily)
-		
-		# QUERY SET ==================
-		if role == constants.VENDOR:
-			vendor_agent = get_object_or_404(VendorAgent, user = request.user)
-			vendor = vendor_agent.vendor
-			queryset_orders = OrderDeliveryStatus.objects.filter(order__vendor__id = vendor.id, date__gte = start_date, date__lte = end_date)
-			queryset_consumers = Consumer.objects.filter(associated_vendor = vendor, user__date_joined__gte = start_date, user__date_joined__lte = end_date)		
-		elif role == constants.OPERATIONS:
-			queryset_orders = OrderDeliveryStatus.objects.filter(date__gte = start_date, date__lte = end_date)
-			queryset_consumers = Consumer.objects.filter(user__date_joined__gte = start_date, user__date_joined__lte = end_date)
-			if vendor_id is not None:
-				queryset_orders = queryset_orders.filter(order__vendor__id = vendor_id)
-				queryset_consumers = queryset_consumers.filter(associated_vendor__id = vendor_id)
-		
-		else:
-			content = {'error':'You dont have permissions.'}
-			return Response(content, status = status.HTTP_400_BAD_REQUEST)
-		
-		total_orders_placed = queryset_orders.count()
-		cancelled_queryset = queryset_orders.filter(Q(order_status='CANCELLED') | Q(order_status='REJECTED'))
-		total_orders_cancelled = cancelled_queryset.count()
-
-		delivered_queryset = queryset_orders.filter(Q(order_status='DELIVERED') | Q(order_status='DELIVERYATTEMPTED'))
-		total_orders_delivered = delivered_queryset.count()
-
-		new_consumers_count = queryset_consumers.count()
-
-		# FOR ORDER COUNT FOR INDIVIDUAL DATES ==================
-		for date in alldates:
-			delivery_status_per_date = queryset_orders.filter(date__year = date.year, 
-				date__month = date.month, 
-				date__day = date.day)
-			orders_placed_count = delivery_status_per_date.count()
-
-			delivery_status_delivered_queryset = delivery_status_per_date.filter(Q(order_status = 'DELIVERED') | Q(order_status = 'DELIVERYATTEMPTED'))
-			orders_delivered_count = delivery_status_delivered_queryset.count()
-			
-			result = {
-			'orders_placed_count':orders_placed_count, 
-			'orders_delivered_count':orders_delivered_count,
-			'date': date.date()
-			}
-
-			orders_graph.append(result)
-
-			for delivery_status in delivery_status_delivered_queryset:
-				try:
-					order = Order.objects.filter(delivery_status = delivery_status).latest('pickup_datetime')	
-				except Exception, e:
-					continue
-				
-				total_cod_amount = total_cod_amount + order.cod_amount
-				total_sales = total_sales + order.total_cost
-
-		
-		content = {
-		'new_customers': new_consumers_count,
-		'total_orders_placed':total_orders_placed,
-		'total_orders_delivered':total_orders_delivered,
-		'total_orders_cancelled':total_orders_cancelled,
-		'total_cod_amount':total_cod_amount,
-		'total_sales':total_sales,
-		'orders':orders_graph 
-		}
-		
-		return Response(content, status = status.HTTP_200_OK)
+	if role == constants.VENDOR:
+		vendor_agent = get_object_or_404(VendorAgent, user = request.user)
+		vendor = vendor_agent.vendor
 	else:
-		content = {'error':'Only vendors can access their analytics.'}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
+		vendor_id = request.data.get('vendor_id')
+		if vendor_id is not None:
+			vendor = get_object_or_404(Vendor, pk = vendor_id)
+		else:
+			pass	
+	# --------------------------------------------------------------------
+
+	# VENDOR FILTERING ---------------------------------------------------
+	if vendor is not None:
+		delivery_status_queryset = OrderDeliveryStatus.objects.filter(order__vendor = vendor)
+	else:
+		delivery_status_queryset = OrderDeliveryStatus.objects.all()
+	# --------------------------------------------------------------------
+
+	# TOTAL COD COLLECTED ------------------------------------------------------------
+	cod_collected_dict = delivery_status_queryset.filter(date__gte = start_date, date__lte = end_date).aggregate(cod_collected = Sum('cod_collected_amount'))
+	cod_collected = cod_collected_dict['cod_collected']
+	# ------------------------------------------------------------------------------
+
+	# DATE FILTERING ---------------------------------------------------------------
+	delivery_status_queryset = delivery_status_queryset.filter(date__gte = start_date, date__lte = end_date)
+	# ------------------------------------------------------------------------------
+	
+	# TOTAL COD TO BE COLLECTED -----------------------------
+	total_cod_dict = Order.objects.filter(delivery_status = delivery_status_queryset).aggregate(total_cod = Sum('cod_amount'))
+	total_cod = total_cod_dict['total_cod']
+    # ------------------------------------------------------------------------------
+
+	# ORDER STATUS FILTERING -------------------------------------------------------
+	total_orders = delivery_status_queryset.count()
+	total_orders_executed = delivery_status_queryset.filter(Q(order_status = 'DELIVERED') | Q(order_status = 'DELIVERYATTEMPTED') | Q(order_status = 'PICKUPATTEMPTED')).count()
+	# ------------------------------------------------------------------------------
+
+	# CREATE DATE RULE -----------------------------------------------------------
+	rule_daily = rrule(DAILY, dtstart = start_date, until = end_date)
+	alldates = list(rule_daily)
+	# ----------------------------------------------------------------------------
+
+	# FOR ORDER COUNT FOR INDIVIDUAL DATES -----------------------------------------
+	fullday_timedelta = timedelta(hours=23, minutes=59)
+	orders_graph = []
+	for date in alldates:
+		day_start = date
+		day_end = day_start + fullday_timedelta
+
+		delivery_status_per_date = delivery_status_queryset.filter(date__gte = day_start, date__lte = day_end)
+		
+		total_orders_per_day = delivery_status_per_date.count()
+		orders_delivered_count = delivery_status_per_date.filter(Q(order_status = 'DELIVERED')).count()
+		orders_delivered_attempted_count = delivery_status_per_date.filter(Q(order_status = 'DELIVERYATTEMPTED')).count()
+		orders_pickup_attempted_count = delivery_status_per_date.filter(Q(order_status = 'PICKUPATTEMPTED')).count()
+		orders_cancelled_count = delivery_status_per_date.filter(Q(order_status = 'CANCELLED')).count()
+		orders_undelivered_count = delivery_status_per_date.filter(Q(order_status = 'ORDER_PLACED') | Q(order_status = 'QUEUED')).count()
+		orders_intransit_count = delivery_status_per_date.filter(Q(order_status = 'INTRANSIT')).count()
+
+		result = {
+		'date': ist_datetime(date).date(),
+		'orders_placed_count':total_orders_per_day, 
+		'orders_delivered_count':orders_delivered_count,
+		'delivery_attempted_count':orders_delivered_attempted_count,
+		'pickup_attempted_count':orders_pickup_attempted_count,
+		'cancelled_count':orders_cancelled_count,
+		'queued_count':orders_undelivered_count,
+		'intransit_count':orders_intransit_count
+		}
+		orders_graph.append(result)
+	# ------------------------------------------------------------------------------
+
+	# CUSTOMERS FOR VENDOR ---------------------------------------------------------
+	queryset_consumers = Consumer.objects.filter(user__date_joined__gte = start_date, user__date_joined__lte = end_date)
+	if vendor is not None:
+		queryset_consumers = queryset_consumers.filter(associated_vendor = vendor)
+	
+	new_customers_count = queryset_consumers.count()
+	# ------------------------------------------------------------------------------
+	
+	content = {
+	'orders':orders_graph,
+	'total_orders_placed':total_orders,
+	'total_orders_delivered':total_orders_executed,
+	'total_cod_amount':total_cod,
+	'total_cod_collected':cod_collected,
+	'new_customers': new_customers_count,
+	'total_orders_cancelled':0,
+	'total_sales':0,
+	}
+	return Response(content, status = status.HTTP_200_OK)
