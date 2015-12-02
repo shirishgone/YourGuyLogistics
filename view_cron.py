c44cc3fa3ec149ad1125aaf810f41ba76879173f
Shirish <shirish542@gmail.com>
2015-12-02 12:33:39 +0530

    Added few more events for running autoassignment

--- view_cron.py
+++ view_cron.py
@@ -10,6 +10,14 @@ from pytz import utc
 
 scheduler = BackgroundScheduler(timezone=utc)
 
+def auto_assign_deliveries():
+	url = 'http://yourguy.herokuapp.com/api/v2/cron/'
+	try:
+		result = requests.get(url)
+	except:
+		print 'Error in Auto Assign'
+		# REPORT ERROR
+
 # AUTO ASSIGNED SCHEDULER ----------------------------------------
 @scheduler.scheduled_job('cron', id='auto_assign_job_id', hour=0)
 def auto_assign():
@@ -32,6 +40,11 @@ def daily_report():
 		print 'send_daily_report : ERROR'
 		# REPORT ERROR
 # ----------------------------------------------------------------
+
+scheduler.add_job(auto_assign_deliveries, id='auto_assign_job_1', hour=4)
+scheduler.add_job(auto_assign_deliveries, id='auto_assign_job_2', hour=6)
+scheduler.add_job(auto_assign_deliveries, id='auto_assign_job_3', hour=8)
+scheduler.add_job(auto_assign_deliveries, id='auto_assign_job_4', hour=11)
 
 scheduler.start()
 
