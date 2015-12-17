6ebe59705de84da3dc249c66926022fbd9a94561
Shirish <shirish542@gmail.com>
2015-12-17 15:58:40 +0530

    Updated utils methods

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -19,16 +19,9 @@ urlpatterns = patterns(
 	url(r'^excel_download/', view_dashboard.excel_download, name = 'excel_download'),
 	url(r'^website_email/', mail.website_email, name = 'website_email'),
 	
-	url(r'^old_order_number_for_new_delivery_id/', utils.old_order_number_for_new_delivery_id, name = 'old_order_number_for_new_delivery_id'),	
-	# url(r'^is_recurring_var_setting/', utils.is_recurring_var_setting, name = 'is_recurring_var_setting'),
-	# url(r'^delivery_status_update/', utils.delivery_status_update, name = 'delivery_status_update'),
-	# url(r'^fill_full_address/', utils.fill_full_address, name = 'fill_full_address'),
-	# url(r'^fill_order_ids/', utils.fill_order_ids, name = 'fill_order_ids'),
-	# url(r'^attach_order_to_deliverystatus/', utils.attach_order_to_deliverystatus, name = 'attach_order_to_deliverystatus'),
-	# url(r'^delivery_status_without_order_id/', utils.delivery_status_without_order_id, name = 'delivery_status_without_order_id'),
-	# url(r'^delivery_status_without_order/', utils.delivery_status_without_order, name = 'delivery_status_without_order'),
-	# url(r'^remove_delivery_status_without_order_ids/', utils.remove_delivery_status_without_order_ids, name = 'remove_delivery_status_without_order_ids'),	
-	# url(r'^delivery_status_for_order_id/', utils.delivery_status_for_order_id, name = 'delivery_status_for_order_id'),	
+	url(r'^new_order_id_for_old_order_id/', utils.new_order_id_for_old_order_id, name = 'new_order_id_for_old_order_id'),
+	url(r'^old_order_id_for_new_order_id/', utils.old_order_id_for_new_order_id, name = 'old_order_id_for_new_order_id'),	
+	url(r'^fill_full_address/', utils.fill_full_address, name = 'fill_full_address'),
 )
 router = DefaultRouter()
 router.register(r'consumer', ConsumerViewSet)
--- api_v2/utils.py
+++ api_v2/utils.py
@@ -41,53 +41,9 @@ def is_consumer_has_same_address_already(consumer, pincode):
 	except:
 		return None
 
-@api_view(['GET'])
-def is_recurring_var_setting(request):
-	if request.user.is_staff is False:
-		content = {
-		'error':'insufficient permissions', 
-		'description':'Only admin can access this method'
-		}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
-	else:
-		all_orders = Order.objects.all()
-		for order in all_orders:
-			if len(order.delivery_status.all()) > 1:
-				order.is_recurring = True
-				order.save()
-	
-		content = {'data':'All done'}
-		return Response(content, status = status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def delivery_status_update(request):
-	if request.user.is_staff is False:
-		content = {
-		'error':'insufficient permissions', 
-		'description':'Only admin can access this method'
-		}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
-	else:
-		
-		all_delivery_statuses = OrderDeliveryStatus.objects.filter(order_status = 'ATTEMPTED')
-		not_updated_deliverys = []		
-		for delivery_status in all_delivery_statuses:
-			try:
-				if delivery_status.order_status == 'ATTEMPTED':
-					delivery_status.order_status = 'DELIVERYATTEMPTED'
-					delivery_status.save()
-			except Exception, e:
-				not_updated_deliverys.append(delivery_status.id)
-				pass		
-		content = {
-		'data':'All done',
-		'not_updated_deliverys':json.dumps(not_updated_deliverys)
-		}
-		return Response(content, status = status.HTTP_200_OK)
 
 @api_view(['POST'])
-def old_order_number_for_new_delivery_id(request):
+def old_order_id_for_new_order_id(request):
 	if request.user.is_staff is False:
 		content = {
 		'error':'insufficient permissions', 
@@ -95,7 +51,7 @@ def old_order_number_for_new_delivery_id(request):
 		}
 		return Response(content, status = status.HTTP_400_BAD_REQUEST)
 	else:
-		delivery_status_id = request.data['delivery_status_id']
+		delivery_status_id = request.data['new_order_id']
 		delivery_status = get_object_or_404(OrderDeliveryStatus, pk = delivery_status_id)
 		old_id = delivery_status.order.id
 		content = {
@@ -103,8 +59,8 @@ def old_order_number_for_new_delivery_id(request):
 		}
 		return Response(content, status = status.HTTP_200_OK)
 
-@api_view(['POST'])
-def delivery_status_for_order_id(request):
+@api_view(['GET'])
+def new_order_id_for_old_order_id(request):
 	if request.user.is_staff is False:
 		content = {
 		'error':'insufficient permissions', 
@@ -112,111 +68,13 @@ def delivery_status_for_order_id(request):
 		}
 		return Response(content, status = status.HTTP_400_BAD_REQUEST)
 	else:
-		order_id = request.data['order_id']
-		delivery_status_id = request.data['delivery_status_id']
-		
-		order = get_object_or_404(Order, pk = order_id)
-		delivery_status = get_object_or_404(OrderDeliveryStatus, pk = delivery_status_id)
-
-		delivery_status.order = order
-		delivery_status.save()
-
+		old_order_id = request.data['old_order_id']
+		order = get_object_or_404(Order, pk = old_order_id)
+		delivery_status = get_object_or_404(OrderDeliveryStatus, order = order)
+		new_id = delivery_status.id
 		content = {
-		'data':'sucess'
+		'new_id':new_id
 		}
-		return Response(content, status = status.HTTP_200_OK)
-
-@api_view(['GET'])
-def delivery_status_without_order(request):
-	if request.user.is_staff is False:
-		content = {
-		'error':'insufficient permissions', 
-		'description':'Only admin can access this method'
-		}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
-	else:
-		delivery_statuses = OrderDeliveryStatus.objects.filter(order = None)
-		count = len(delivery_statuses)
-		delivery_status_ids = []
-		for delivery_status in delivery_statuses:
-			delivery_status_ids.append(delivery_status.id)
-		
-		content = {
-		'count':count,
-		'ids':delivery_status_ids
-		}
-		return Response(content, status = status.HTTP_200_OK)
-
-@api_view(['GET'])
-def delivery_status_without_order_id(request):
-	if request.user.is_staff is False:
-		content = {
-		'error':'insufficient permissions', 
-		'description':'Only admin can access this method'
-		}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
-	else:
-		count = OrderDeliveryStatus.objects.filter(order_id_in_order_table = 0).count()
-		content = {
-		'count':count
-		}
-		return Response(content, status = status.HTTP_200_OK)
-
-@api_view(['GET'])
-def attach_order_to_deliverystatus(request):
-	if request.user.is_staff is False:
-		content = {
-		'error':'insufficient permissions', 
-		'description':'Only admin can access this method'
-		}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
-	else:
-		all_delivery_status = OrderDeliveryStatus.objects.filter(order = None, order_id_in_order_table__gt = 0)
-		mini_delivery_statuses = all_delivery_status[0:2000]
-		for delivery_status in mini_delivery_statuses:
-			try:
-				order = get_object_or_404(Order, pk = delivery_status.order_id_in_order_table)
-				delivery_status.order = order
-				delivery_status.save()
-			except Exception, e:
-				pass
-		content = {'data':'Done attaching orders'}
-		return Response(content, status = status.HTTP_200_OK)
-
-@api_view(['GET'])
-def remove_delivery_status_without_order_ids(request):
-	if request.user.is_staff is False:
-		content = {
-		'error':'insufficient permissions', 
-		'description':'Only admin can access this method'
-		}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
-	else:
-		all_delivery_status = OrderDeliveryStatus.objects.filter(order_id_in_order_table = 0)
-		updating_delivery_statuses = all_delivery_status[0:1000]
-		for delivery_status in updating_delivery_statuses:
-			delivery_status.delete()
-		content = {'data':'Done saving order ids'}
-		return Response(content, status = status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-def fill_order_ids(request):
-	if request.user.is_staff is False:
-		content = {
-		'error':'insufficient permissions', 
-		'description':'Only admin can access this method'
-		}
-		return Response(content, status = status.HTTP_400_BAD_REQUEST)
-	else:		
-		all_orders = Order.objects.filter(delivery_status__order_id_in_order_table = 0).prefetch_related('delivery_status')
-		updating_orders = all_orders[0:1000]
-		for order in updating_orders:
-			all_deliveries = order.delivery_status.filter(order_id_in_order_table = 0)
-			for delivery_status in all_deliveries:
-				delivery_status.order_id_in_order_table = order.id
-				delivery_status.save()
-		content = {'data':'Done saving order ids'}
 		return Response(content, status = status.HTTP_200_OK)
 
 @api_view(['GET'])
