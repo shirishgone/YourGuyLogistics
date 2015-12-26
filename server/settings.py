140e98585a5558e02c126e970493736604ea6420
Prajakta Joshi <prajaktajoshi@Prajakta-MacBook-Pro.local>
2015-12-26 11:24:58 +0530

    Created a new cron_jobs file, updated view_cron to call new cron_jobs file and uncommented settings

--- server/settings.py
+++ server/settings.py
@@ -157,10 +157,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# # Parse database configuration from $DATABASE_URL
-# if ENVIRONMENT != 'LOCAL':
-#     import dj_database_url
-#     DATABASES['default'] = dj_database_url.config()
+# Parse database configuration from $DATABASE_URL
+if ENVIRONMENT != 'LOCAL':
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
