90c5c721c877ab8d82efd4304fc47f39f776728f
Shirish <shirish542@gmail.com>
2016-01-12 22:08:05 +0530

    product category added to the product model

--- yourguy/models.py
+++ yourguy/models.py
@@ -296,7 +296,7 @@ class Product(models.Model):
     cost = models.FloatField(default = 0.0)
     vendor = models.ForeignKey(Vendor, blank = True, null = True)
     timeslots = models.ManyToManyField(TimeSlot, blank = True)
-    
+    product_category = models.ForeignKey(ProductCategory, blank = True, null = True)
     def __unicode__(self):
         return u"%s - %s" % (self.name, self.vendor)
 
