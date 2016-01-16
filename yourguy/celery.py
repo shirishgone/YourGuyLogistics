78e64f1a26b067a97ae16ae395eade4c3541cec5
Prajakta Joshi <prajaktajoshi@Prajakta-MacBook-Pro-2.local>
2016-01-16 15:03:25 +0530

    celery changes for stage

--- yourguy/celery.py
+++ yourguy/celery.py
@@ -10,7 +10,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
 
 from django.conf import settings
 
-app = Celery('yourguy', broker = settings.BROKER_URL)
+app = Celery('yourguy')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
