91bef34aaef0eb7c60ad91092120e2b08ef09b78
Shirish <shirish542@gmail.com>
2016-02-09 12:16:27 +0530

    trying out a fix to collect static files issue on heroku

--- server/settings.py
+++ server/settings.py
@@ -191,7 +191,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/webapp/static'),
 )
 
 XS_SHARING_ALLOWED_ORIGINS = "*"
