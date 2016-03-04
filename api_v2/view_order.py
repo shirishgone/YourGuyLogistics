a2cdcdb2f8185fc139f108e6144410aae86a7629
Shirish <shirish542@gmail.com>
2016-03-04 17:29:53 +0530

    place_order consumer arch changes bug fix

--- api_v2/view_order.py
+++ api_v2/view_order.py
@@ -94,12 +94,12 @@ def retail_order_send_email(vendor, new_order_ids):
     body = body + '\n\nThanks \n-YourGuy BOT'
     send_email(constants.RETAIL_EMAIL_ID, subject, body)
 
-def fetch_consumer(consumer_phone_number, consumer_name, vendor):
+def fetch_consumer(phone_number, name, vendor):
     try:
-        consumer = Consumer.objects.get(phone_number=consumer_phone_number)    
+        consumer = Consumer.objects.get(phone_number=phone_number)    
     except Exception as e:
         user = User.objects.create(username = phone_number)
-        consumer = Consumer.objects.create(user = user, phone_number=consumer_phone_number, full_name = consumer_name)
+        consumer = Consumer.objects.create(user = user, phone_number=phone_number, full_name = name)
     consumer.associated_vendor.add(vendor)
     consumer.save()
     return consumer
