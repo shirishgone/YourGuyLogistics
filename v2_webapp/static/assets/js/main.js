440671b332085310b2b1fd4023c9de2da573796e
shahwarcoder <aquid.shahwar@gmail.com>
2016-03-19 15:40:48 +0530

    feedback module finished

--- v2_webapp/static/assets/js/main.js
+++ v2_webapp/static/assets/js/main.js
@@ -710,7 +710,7 @@
 			}),
 			addNotes : $resource(constants.v3baseUrl+'freshdesk/add_note/',{},{
 				update :{
-					method: 'PUT',
+					method: 'POST',
 				}
 			}),
 			resolve : $resource(constants.v3baseUrl+'freshdesk/resolve/',{},{
@@ -3119,7 +3119,7 @@
 })();
 (function(){
 	'use strict';
-	var feedbackDetailCntrl = function($state,$stateParams,Feedback,ticket,groups,Notification,PreviousState){
+	var feedbackDetailCntrl = function($state,$stateParams,$mdDialog,Feedback,ticket,groups,Notification,PreviousState,UserProfile,constants){
 		console.log(ticket);
 		var self =  this;
 		this.params = $stateParams;
@@ -3148,7 +3148,60 @@
 				}
 			}
 		};
-
+		/*
+			@getUsername function that returns the name of the user who has updated the note
+		*/
+		this.getUsername =  function(id){
+			if(id == self.ticket.requester_id){
+				return self.ticket.requester_name;
+			}
+			else{
+				return self.ticket.responder_name;
+			}
+		};
+		this.showConfirm = function(ev) {
+		    // Appending dialog to document.body to cover sidenav in docs app
+		    var confirm = $mdDialog.confirm()
+		    .title('Are you sure you want to resolve this issue')
+		    .ariaLabel('Resolve Feedback')
+		    .targetEvent(ev)
+		    .ok('Confirm')
+		    .cancel('Cancel');
+		    $mdDialog.show(confirm).then(function() {
+		    	self.closeComplain({});
+		    });
+		};
+		/*
+			@addNotes function to add a note for the feedback
+		*/
+		this.addNotes = function(data){
+			Notification.loaderStart();
+			data.id = self.ticket.display_id;
+			if(UserProfile.$getUserRole() === constants.userRole.VENDOR){
+				data.note.helpdesk_note.user_id = self.ticket.requester_id;
+			}
+			else{
+				data.note.helpdesk_note.user_id = self.ticket.responder_id;
+			}
+			Feedback.addNotes.update(data,function(response){
+				self.getTicket();
+			});
+		};
+		/*
+			@closeComplain function to close the feedback
+		*/
+		this.closeComplain = function(data){
+			Notification.loaderStart();
+			data.id = self.ticket.display_id;
+			data.resolve = {
+				helpdesk_ticket :{
+					status: "5"
+				}
+			};
+			Feedback.resolve.update(data,function(response){
+				self.getTicket();
+			});
+		};
 		/*
 			@getTickets rleoads the tickets controller according too the filter to get the new filtered data.
 		*/
@@ -3162,11 +3215,14 @@
 	.controller('feedbackDetailCntrl', [
 		'$state',
 		'$stateParams',
+		'$mdDialog',
 		'Feedback',
 		'ticket',
 		'groups',
 		'Notification',
 		'PreviousState',
+		'UserProfile',
+		'constants',
 		feedbackDetailCntrl
 	]);
 })();
