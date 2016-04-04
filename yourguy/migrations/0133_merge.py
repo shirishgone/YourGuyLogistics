c0b949f9c60143d7758a49081ac71b27269517ff
Prajakta Joshi <prajaktajoshi@Prajakta-MacBook-Pro.local>
2016-04-04 13:11:10 +0530

    Adding the merged migration

---
+++ yourguy/migrations/0133_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('yourguy', '0132_codtransaction_salary_deduction'),
+        ('yourguy', '0132_deliveryguy_associated_vendors'),
+    ]
+
+    operations = [
+    ]
